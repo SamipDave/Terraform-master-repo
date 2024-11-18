f6bd47e68e983b9778bdcddf0da7fb5918641d56
SamipDave <118797360+SamipDave@users.noreply.github.com>
2024-11-18 23:46:35 +0530

    te

---
+++ local.tf
@@ -0,0 +1,8 @@
+locals {
+    env             = "My-vpc"
+    region          = "us-east-1"
+    zone1           = "us-east-1a"
+    zone2           = "us-east-2a"
+    eks_name        = "My-eks-cluster"
+    eks_version     = "1.30"
+}
