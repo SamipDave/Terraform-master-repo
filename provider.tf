6011c839b9ad975fcbfb3a6adb259260fe24706c
SamipDave <118797360+SamipDave@users.noreply.github.com>
2024-11-21 23:29:53 +0530

    providers added

--- provider.tf
+++ provider.tf
@@ -1,1 +1,14 @@
-provider aws
+provider "aws" {
+    region = local.region
+}
+
+terraform {
+    required_version = "> 1.0"
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.53"
+        }
+    }
+}
