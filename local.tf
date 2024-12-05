b2975f216a0a544ba4cd969e0a438ef8ef10fe70
SamipDave <118797360+SamipDave@users.noreply.github.com>
2024-12-05 07:16:21 +0000

    Delete local.tf

--- local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-    env             = "My-vpc"
-    region          = "us-east-1"
-    zone1           = "us-east-1a"
-    zone2           = "us-east-2a"
-    eks_name        = "My-eks-cluster"
-    eks_version     = "1.31"
-}
